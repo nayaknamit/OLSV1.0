ff02cb2347581ce4642a92408f2f8a64b9011a8f
Ashish Arora <aashi.arora@gmail.com>
2015-02-27 23:51:19 +0530

    Added user model

---
+++ OLS/OLS/DataModel/user.h
@@ -0,0 +1,14 @@
+//
+//  user.h
+//  OLS
+//
+//  Created by Ashish Arora 4 on 2/27/15.
+//  Copyright (c) 2015 Sapient. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface user : NSObject
+@property(nonatomic,assign) NSInteger userID;
+@property(nonatomic,strong) NSString *userName;
+@end
---
+++ OLS/OLS/DataModel/user.m
@@ -0,0 +1,13 @@
+//
+//  user.m
+//  OLS
+//
+//  Created by Ashish Arora 4 on 2/27/15.
+//  Copyright (c) 2015 Sapient. All rights reserved.
+//
+
+#import "user.h"
+
+@implementation user
+
+@end
