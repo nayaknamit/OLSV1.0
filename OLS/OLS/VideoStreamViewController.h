524422ea887611734b7ba81d57389b8b174730e1
Hackathon <namitnayak@gmail.com>
2015-02-28 01:09:21 +0530

    added videostream view controller class and xib file

---
+++ OLS/OLS/VideoStreamViewController.h
@@ -0,0 +1,13 @@
+//
+//  VideoStreamViewController.h
+//  OLS
+//
+//  Created by Hackathon on 2/28/15.
+//  Copyright (c) 2015 Sapient. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface VideoStreamViewController : ViewController
+
+@end
